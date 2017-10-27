a4115ca09390651677626883306ebd1cd5262dcd
Robc <robczepil@gmail.com>
2017-10-26 23:06:14 -0700

    Add more buttons to pad.py

--- bluepad/pad.py
+++ bluepad/pad.py
@@ -11,14 +11,40 @@ def data_received(data):
     #dpad up commands
     if command == "1":
         print ("up_pressed")
-    elif command == "0":
+    elif command == "2":
         print ("up_released")
+        
     #dpad down commands
-    elif command == "4":
-        print ("down_pressed")
     elif command == "3":
+        print ("down_pressed")
+    elif command == "4":
         print ("down_released")
+        
+    #dpad left commands
+    elif command == "5":
+        print ("left_pressed")
+    elif command == "6":
+        print ("left_released")
+        
+    #dpad right commands
+    elif command == "7":
+        print ("right_pressed")
+    elif command == "8":
+        print ("right_released")
+
+    #manual commands
+    elif command == "9":
+        print ("manual_pressed")
+    elif command == "10":
+        print ("manual_released")
+
+    #auto commands
+    elif command == "11":
+        print ("auto_pressed")
+    elif command == "12":
+        print ("auto_released")    
 
 
 BluetoothServer(data_received)
 pause()
+
